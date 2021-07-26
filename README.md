448d7f49130dd61c72725e263bfdfc0506ce96ab
Prabir Shrestha <mail@prabir.me>
2021-07-25 23:55:52 -0700

    add README.md

--- README.md
+++ README.md
@@ -2,3 +2,69 @@ trillium-ructe
 ==============
 
 [ructe](https://crates.io/crates/ructe) templates for [trillium.rs](https://trillium.rs/).
+
+# Getting Started
+
+```toml
+[package]
+name = "example-ructe-template"
+edition = "2018"
+build = "src/build.rs"
+
+[dependencies]
+trillium = "0.1.4"
+trillium-ructe = "0.1.1"
+trillium-smol = "0.1.3"
+
+[build-dependencies]
+ructe = { version = "0.13.4", features = ["sass"] }
+```
+
+# Example
+
+## src/build.rs
+
+```rust
+use ructe::{Result, Ructe};
+
+fn main() -> Result<()> {
+    let mut ructe = Ructe::from_env()?;
+    let mut statics = ructe.statics()?;
+    statics.add_files("static")?;
+    statics.add_sass_file("styles/style.scss")?;
+    ructe.compile_templates("templates")?;
+    Ok(())
+}
+```
+
+## templates/helloworld.rs.html
+
+```html
+@(text: &str)
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Trillium Ructe Example</title>
+    </head>
+    <body>
+        <h1>@text</h1>
+    </body>
+</html>
+```
+
+## src/main.rs
+
+```rust
+use trillium::Conn;
+use trillium_ructe::render_html_try;
+
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
+
+fn main() {
+    trillium_smol::run(|conn: Conn| async move {
+        render_html_try!(conn, |o| templates::helloworld(o, "html"))
+    });
+}
+```
+
+Use `render_html_try` to render html or `render_try` to render raw template.
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,108 @@
 #![forbid(unsafe_code)]
 
-use std::io::{Result, Write};
+use std::io::Write;
 use trillium::Conn;
 
-pub trait RucteConnExt {
-    fn render<F>(self, do_render: F) -> Result<Self>
-    where
-        Self: Sized,
-        F: FnOnce(&mut dyn Write) -> Result<()>;
-
-    fn render_html<F>(self, do_render: F) -> Result<Self>
-    where
-        Self: Sized,
-        F: FnOnce(&mut dyn Write) -> Result<()>;
+#[derive(thiserror::Error, Debug)]
+pub enum RucteError {
+    #[error("Failed to render ructe template")]
+    IoError {
+        #[source]
+        source: std::io::Error,
+        conn: Conn,
+    },
 }
 
-impl RucteConnExt for Conn {
-    fn render<F>(self, do_render: F) -> Result<Self>
-    where
-        Self: Sized,
-        F: FnOnce(&mut dyn Write) -> Result<()>,
-    {
-        let mut buf = Vec::new();
-        match do_render(&mut buf) {
-            Ok(()) => Ok(self.ok(buf)),
-            Err(e) => Err(e),
-        }
+pub fn render<F>(conn: Conn, call: F) -> Result<Conn, RucteError>
+where
+    F: FnOnce(&mut dyn Write) -> std::io::Result<()>,
+{
+    let mut buf = Vec::new();
+    match call(&mut buf) {
+        Ok(()) => Ok(conn.ok(buf)),
+        Err(source) => Err(RucteError::IoError { source, conn }),
     }
+}
 
-    fn render_html<F>(self, do_render: F) -> Result<Self>
-    where
-        Self: Sized,
-        F: FnOnce(&mut dyn Write) -> Result<()>,
-    {
-        Ok(self
-            .with_header(("content-type", "text/html; charset=utf-8"))
-            .render(do_render)?)
-    }
+pub fn render_html<F>(conn: Conn, call: F) -> Result<Conn, RucteError>
+where
+    F: FnOnce(&mut dyn Write) -> std::io::Result<()>,
+{
+    render(
+        conn.with_header(("content-type", "text/html; charset=utf-8")),
+        call,
+    )
+}
+
+/**
+Renders a template or returns the conn with a 500 status.
+
+```ignore
+use trillium::Conn;
+use trillium_ructe::render_html_try;
+
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
+
+fn main() {
+    trillium_smol::run(|conn: Conn| async move {
+        // helloworld.rs.html contents:
+        //  @(text: &str)
+        //  <h1>@text</h1>
+        render_try!(conn, |o| templates::helloworld(o, "hello world"))
+    });
+}
+```
+*/
+#[macro_export]
+macro_rules! render_try {
+    ($conn:expr, $expr:expr) => {
+        match $crate::render($conn, $expr) {
+            Ok(conn) => conn,
+            Err(e) => match e {
+                $crate::RucteError::IoError { source, conn } => {
+                    trillium::log::error!("{}:{} render_try error: {}", file!(), line!(), source);
+                    return conn.with_status(500).halt();
+                }
+            },
+        }
+    };
+}
+
+/**
+Renders a template and sets content-type as "text/html; charset=utf-8" or returns the conn with a 500 status.
+```ignore
+use trillium::Conn;
+use trillium_ructe::render_html_try;
+
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
+
+fn main() {
+    trillium_smol::run(|conn: Conn| async move {
+        // helloworld.rs.html contents:
+        //  @(text: &str)
+        //  <h1>@text</h1>
+        render_html_try!(conn, |o| templates::helloworld(o, "hello world"))
+    });
+}
+
+```
+*/
+#[macro_export]
+macro_rules! render_html_try {
+    ($conn:expr, $expr:expr) => {
+        match $crate::render_html($conn, $expr) {
+            Ok(conn) => conn,
+            Err(e) => match e {
+                $crate::RucteError::IoError { source, conn } => {
+                    trillium::log::error!(
+                        "{}:{} render_html_try error: {}",
+                        file!(),
+                        line!(),
+                        source
+                    );
+                    return conn.with_status(500).halt();
+                }
+            },
+        }
+    };
 }
